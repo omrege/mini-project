192b7bc6cb1f4765832050b0392f0cb781f6de65
omrege <81291326+omrege@users.noreply.github.com>
2021-04-16 15:25:49 +0530

    Add files via upload

---
+++ 3_Implementation/inc/calc.h
@@ -0,0 +1,42 @@
+/**
+ * @file calc.h
+ * @author Om Rege (regeom.ec17@bmsce.ac.in)
+ * @brief  Program to perform operations on numbers
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __OPERATIONS
+#define __OPERATIONS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief perform addition 
+ * 
+ */
+int add(int n1, int n2);
+
+/**
+ * @brief perform subtraction
+ * 
+ */
+int subtract(int n1, int n2);
+
+/**
+ * @brief perform multiplication
+ * 
+ */
+int multiply(int n1, int n2);
+
+
+/**
+ * @brief perform division
+ * 
+ */
+int divide(int n1, int n2);
+
+#endif
