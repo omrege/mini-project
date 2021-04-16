a6029a168c208b32030229e2171f721bb368442f
omrege <81291326+omrege@users.noreply.github.com>
2021-04-16 15:27:26 +0530

    Add files via upload

---
+++ 3_Implementation/src/calc.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include "calc.h"
+ 
+int add(int n1, int n2)
+{   
+    int a,b,total;
+    a=n1;b=n2;
+    total=a+b;
+    return total;
+}
+ 
+int subtract(int n1, int n2)
+{ 
+    int a,b,total;
+    a=n1;b=n2;
+    total=a-b; 
+    return total;
+}
+ 
+int multiply(int n1, int n2)
+{
+    int a,b,total;
+    a=n1;b=n2;
+    total=a*b; 
+    return total;
+}
+ 
+int divide(int n1, int n2)
+{
+    int a,b,total;
+    a=n1;b=n2;
+    total=a/b;
+    return total;
+}
+ 
